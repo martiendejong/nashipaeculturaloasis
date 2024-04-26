abd9a0dfea193d940019026a16e2463e17265ba4
Martien de Jong <martiendejong2008@gmail.com>
2024-04-26 16:18:43 +0200

    update

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -4,13 +4,15 @@ import { FullImgComponent } from "../full-img/full-img.component";
 import { NgFor, NgIf } from "@angular/common";
 import { CarouselFaderComponent } from "../carousel-fader/carousel-fader.component";
 import { JsonLoaderService } from '../json-loader.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-activities',
     standalone: true,
     templateUrl: './activities.component.html',
     styleUrls: ['./activities.component.scss', '../page.scss'],
-    imports: [CarouselCycleComponent, FullImgComponent, NgFor, NgIf, CarouselFaderComponent]
+    imports: [CarouselCycleComponent, FullImgComponent, NgFor, NgIf, CarouselFaderComponent, RouterModule],
+    providers: [RouterModule]
 })
 export class ActivitiesComponent implements OnInit {
     @Input() src: any;
