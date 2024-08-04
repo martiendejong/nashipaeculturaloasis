e6673855f4cb5556c2a9eb64f0590f966b5ec699
RESIATO <RESIATO@DESKTOP-RH2MLRF>
2024-08-05 01:17:26 +0300

    We have put the content of the about page in a json file

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { JsonLoaderService } from '../json-loader.service';
+import { CommonModule, NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,NgFor],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss', '../page.scss']
 })
 export class AboutComponent {
+  @Input() src: any;
+
+  public content: any;
+
+  constructor(private jsonLoaderService: JsonLoaderService) { }
+
+  async ngOnInit() {
+    debugger
+    this.content = await this.jsonLoaderService.loadJson(this.src);
+  }
 
 }
