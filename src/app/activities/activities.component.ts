b48aa026d6e42e0368936d661ab22b66fb4e7c49
Lesiamon <lesiamon.mpoe@student.moringaschool.com>
2024-08-14 16:16:39 +0300

    changed background images on safari page, added more images, categorised images on safari page.

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CarouselCycleComponent } from "../carousel-cycle/carousel-cycle.component";
 import { FullImgComponent } from "../full-img/full-img.component";
-import { NgFor, NgIf } from "@angular/common";
+import { NgFor, NgIf, NgClass } from "@angular/common";  // Make sure NgClass is imported
 import { CarouselFaderComponent } from "../carousel-fader/carousel-fader.component";
 import { JsonLoaderService } from '../json-loader.service';
 import { RouterModule } from '@angular/router';
@@ -11,7 +11,7 @@ import { RouterModule } from '@angular/router';
     standalone: true,
     templateUrl: './activities.component.html',
     styleUrls: ['./activities.component.scss', '../page.scss'],
-    imports: [CarouselCycleComponent, FullImgComponent, NgFor, NgIf, CarouselFaderComponent, RouterModule],
+    imports: [CarouselCycleComponent, FullImgComponent, NgFor, NgIf, NgClass, CarouselFaderComponent, RouterModule],
     providers: [RouterModule]
 })
 export class ActivitiesComponent implements OnInit {
@@ -23,5 +23,20 @@ export class ActivitiesComponent implements OnInit {
   
     async ngOnInit() {
       this.content = await this.jsonLoaderService.loadJson(this.src);
+    }
+
+    // Method to return a specific background class based on the content item
+    getBackgroundClass(index: number): string {
+        const classes = [
+            'activities-one',
+            'activities-two',
+            'activities-three',
+            'activities-four',
+            'activities-five',
+            'activities-six',
+            'activities-seven',
+            'activities-eight'
+        ];
+        return classes[index % classes.length];
     }
 }
