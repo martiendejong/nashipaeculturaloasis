1bbd753557b1ec28ebbd79a65cf0eda666645622
FRANK <RESIATO@DESKTOP-RH2MLRF>
2024-08-14 13:00:26 +0300

    added images

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { JsonLoaderService } from '../json-loader.service';
 import { CommonModule, NgFor } from '@angular/common';
+import { FullImgComponent } from "../full-img/full-img.component";
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule,NgFor],
+  imports: [CommonModule, NgFor, FullImgComponent],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss', '../page.scss']
 })
