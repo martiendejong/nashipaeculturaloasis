cdfd94364e072095f04e6ad22b775a954fade4ae
Martien de Jong <martiendejong2008@gmail.com>
2024-11-19 23:53:03 +0100

    flag v1

--- angular-website/src/app/app.config.ts
+++ angular-website/src/app/app.config.ts
@@ -1,10 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule, HttpHandler, provideHttpClient, withFetch } from '@angular/common/http';
 
+// Define a token for injecting the default language
+export const DEFAULT_LANGUAGE = new InjectionToken<string>('DefaultLanguage');
+
+// Get the default language from local settings
+const navigatorLanguage = 'en';// typeof navigator !== 'undefined' ? navigator.language.slice(0, 2) : 'en';
+const defaultLanguage = ['en', 'fr', 'de'].includes(navigatorLanguage) ? navigatorLanguage : 'en';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    { provide: DEFAULT_LANGUAGE, useValue: defaultLanguage }
+  ]
 };
--- angular-website/src/app/menu/menu.component.ts
+++ angular-website/src/app/menu/menu.component.ts
@@ -2,18 +2,29 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { Scroll } from '../scroll.service';
 import { RouterModule, provideRouter } from '@angular/router';
 import { routes } from '../app.routes';
+import { NgFor, NgIf }from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgFor, NgIf],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss',
+  styleUrls: ['./menu.component.scss'],
   providers: [Scroll, RouterModule]
 })
 export class MenuComponent implements OnInit {
+  public showLanguageMenu: boolean = false;
+  public languages = [
+    { code: 'en', name: 'English' },
+    { code: 'fr', name: 'French' },
+    { code: 'de', name: 'German' }
+  ];
+  public selectedLanguage: string;
+
   constructor(public scroll: Scroll) {
+    this.selectedLanguage = 'en';// navigator !== undefined ? navigator.language.slice(0, 2) : 'en';
   }
+
   ngOnInit(): void {
     this.scroll.onWindowScroll();
   }
@@ -21,5 +32,15 @@ export class MenuComponent implements OnInit {
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.scroll.onWindowScroll();
+  }
+
+  toggleLanguageMenu() {
+    this.showLanguageMenu = !this.showLanguageMenu;
+  }
+
+  changeLanguage(languageCode: string) {
+    this.selectedLanguage = languageCode;
+    this.showLanguageMenu = false;
+    // Logic to change the language settings goes here.
   }
 }
